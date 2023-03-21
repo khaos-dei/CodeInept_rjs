c84b9a43d9e861de85f0f63d109f17361430aa8b
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-03-22 09:41:19 +1000

    Real-time clock
    24h version, seemignly works, stuffs gonna be moved

--- inept/src/App.js
+++ inept/src/App.js
@@ -10,9 +10,6 @@ import DigiClock from './components/digi-clck.js';
 function App() {
   return (
     <div className="App">
-      {/*<header className="App-header">
-       Calendar Line
-      </header>*/}
       <body className="App-body">
         <div className='nice-box' style={{ 'grid-column-start': 'span 5'}}>Month</div>
         <div className='nice-box' style={{'grid-area': '2/1/span 2/span 1'}}>Clock</div>
@@ -29,38 +26,6 @@ function App() {
         <div className='nice-box' style={{}}>Icons</div>
         <div className='nice-box' style={{}}>Spinwheel</div>
         <div className='nice-box' style={{}}>Youtube</div>
-        {/*
-        <Container>
-          <Row>
-            <Col>
-              <Row>
-                <Col><div className='meow' style={{ height: 180, width: 180 }}>Clock</div></Col>
-                <Col><div className='meow' style={{ height: 180, width: 140 }}>Date</div></Col>
-              </Row>
-              <Row>
-                <Col><div className='meow' style={{ height: 100, width: 180 }}>Digital Clock</div></Col>
-                <Col><div className='meow' style={{ height: 70, width: 50 }}>But</div></Col>
-                <Col><div className='meow' style={{ height: 70, width: 50 }}>But</div></Col>
-              </Row>
-              <Row>
-                <Col><div className='meow' style={{ height: 300, width: 250 }}>Click Fund</div></Col>
-              </Row>
-            </Col>
-            <Col><div className='meow' style={{ height: 150, width: 300 }}>Triage</div></Col>
-            <Col>
-              <Row><Col><div className='meow' style={{ height: 350, width: 300 }}>Notepad</div></Col> </Row>
-              <Row><Col><div className='meow' style={{ height: 150, width: 150 }}>Zen Gif</div></Col> </Row>
-              <Row><Col><div className='meow' style={{ height: 100, width: 300 }}>Music</div></Col> </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>Projects</Col>
-            <Col>Icons</Col>
-            <Col>Spinwheel</Col>
-            <Col>Youtube</Col>
-          </Row>
-        </Container>
-  */}
       </body>
     </div>
   );
--- inept/src/components/digi-clck.js
+++ inept/src/components/digi-clck.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { render } from 'react-dom';
 import './digi-clck.css';
 
 function DigiClock() {
+    const [dateState, setDateState] = useState(new Date());
+    useEffect(() => {
+        setInterval(() => setDateState(new Date()), 30000);//new state every 30 seconds(cannot set it to a min, the app may be started at the latter half of a second)
+    }, []);//all of this wl later get moved to the broader "right up" function
+
+    var hrs = dateState.getHours();//wlb func input
+    hrs = ((hrs < 10) ? '0' + hrs : hrs);//yes, two lines, but avoid double .getHours() call
+    var mins = dateState.getMinutes();//wlb func input
+    var time = hrs + ':' + mins;
     return (
-     <div className='a-box'>16:32</div>
+     <div className='a-box'>{time}</div>
     );
   }
 
