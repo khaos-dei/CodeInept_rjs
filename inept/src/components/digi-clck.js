751ab10bf9cb151ad809a6fdca6c5dbb5371751f
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-03-22 11:30:44 +1000

    smol fix

--- inept/src/components/digi-clck.js
+++ inept/src/components/digi-clck.js
@@ -11,6 +11,7 @@ function DigiClock() {
     var hrs = dateState.getHours();//wlb func input
     hrs = ((hrs < 10) ? '0' + hrs : hrs);//yes, two lines, but avoid double .getHours() call
     var mins = dateState.getMinutes();//wlb func input
+    mins = ((mins < 10) ? '0' + mins : mins);//yes, two lines, but avoid double .getHours() call
     var time = hrs + ':' + mins;
     return (
      <div className='a-box'>{time}</div>
