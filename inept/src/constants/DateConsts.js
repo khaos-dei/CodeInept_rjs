13ec49a79e0c11ee23cc05a08205e8f16d622bf8
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-03-23 15:56:45 +1000

    Calendar mockup

---
+++ inept/src/constants/DateConsts.js
@@ -0,0 +1,31 @@
+const months = [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+const dates = [
+      '1st','2nd','3rd','4th','5th','6th','7th','8thrd','9th','10th',
+      '11tt','12td','13td','14th','15th','16th','17th','18thrd','19th','20th',
+      '21st','22nd','23rd','24th','25th','26th','27th','28thrd','29th','30th',
+      '31st',
+    ];
+
+  export {dates, weekdays, months};
