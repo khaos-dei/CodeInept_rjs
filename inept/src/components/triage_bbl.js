566b8dda2bf7506711239d2222cc42c6765a392c
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-04-10 17:22:56 +1000

    Text overflow clip

--- inept/src/components/triage_bbl.js
+++ inept/src/components/triage_bbl.js
@@ -7,9 +7,11 @@ function Triage_Bubble(props) {
     return (
         <div className='Triage_Bubble'>
             <div className='wrapper-parent'>
-                <div class="Triage_Tag">Triage</div>
-                <div class="Triage_Text" contenteditable="true">
-                    "Be at miss or each good play home they. It leave taste mr in it fancy. She son lose does fond bred gave lady get. Sir her company conduct expense bed any."
+                
+                <div class="Triage_Text" >
+                <div class="Triage_Tag">Triage</div> 
+                <div contenteditable="true"> "Be at miss or each good play home they. It leave taste mr in it fancy. She son lose does fond bred gave lady get. Sir her company conduct expense bed any."
+                </div>
                 </div>
             </div>
         </div>
