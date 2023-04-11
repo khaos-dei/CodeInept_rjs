f32c784cede7c98504395c3127301eea5cf51411
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-04-11 17:23:00 +1000

    saving text to variable

--- inept/src/components/triage_bbl.js
+++ inept/src/components/triage_bbl.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react'
 import './triage_bbl.css';
 
 function Triage_Bubble(props) {
-    //const [triageState, setTriageState] = useState(new Triage());
+    const [triageState, setTriageState] = useState('1');
+    const [triage1State, setTriage1State] = useState('1');
+    const [fontState, setFontState] = useState(4);
     //
+    function dfontSizing(event){
+        setTriageState(event.currentTarget.textContent);
+        setFontState(fontState-0.1);
+    }
     return (
         <div className='Triage_Bubble'>
             <div className='wrapper-parent'>
-                
-                <div class="Triage_Text" >
-                <div class="Triage_Tag">Triage</div> 
-                <div contenteditable="true"> "Be at miss or each good play home they. It leave taste mr in it fancy. She son lose does fond bred gave lady get. Sir her company conduct expense bed any."
-                </div>
-                </div>
+                <div class="Triage_Tag" contentEditable="true">{triageState}</div> 
+                <div class="Triage_Text" contentEditable="true" onKeyUp={dfontSizing} style={{fontSize:`${fontState}vmin`}}>{triageState}</div>
             </div>
         </div>
         
