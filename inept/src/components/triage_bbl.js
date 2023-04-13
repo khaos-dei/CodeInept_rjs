88cc1de3f26f435cc6a437f093d05ff42a47d35d
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-04-13 11:06:00 +1000

    reroute checkpoint
    Sometimes, you commit to a design decision, and you fight and you fight and you fight to make it work until you arrive at a problem that makes you reevaluate your commitment and change course. This is that point

--- inept/src/components/triage_bbl.js
+++ inept/src/components/triage_bbl.js
@@ -1,21 +1,73 @@
 import React, { useEffect, useState } from 'react'
 import './triage_bbl.css';
 
+
+
+//TODO: Add a warning label when user input more than 310
+
 function Triage_Bubble(props) {
-    const [triageState, setTriageState] = useState('1');
-    const [triage1State, setTriage1State] = useState('1');
-    const [fontState, setFontState] = useState(4);
-    //
+    const [singleLinePad, setsingleLinePad] = useState('5vmin');
+    const [triageState, setTriageState] = useState(1);
+    const [triageTextState, setTriageTextState] = useState(['Priority High ', 'Priority Mid ', 'Priority eh ']);
+    const [textLengthState, settextLengthState] = useState(1);
+    const [fontState, setFontState] = useState(8);
+    function sizingFormula(text){
+        if(text<18){
+            setsingleLinePad('5vmin');
+            switch(text){
+                case 10:
+                    return(7.6);
+                case 11:
+                    return(7.2);
+                case 12:
+                    return(6.6);
+                case 13:
+                    return(6);
+                case 14:
+                    return(5.6);
+                case 15:
+                    return(5.2);
+                case 16:
+                    return(4.9);
+                case 17:
+                    return(4.5);
+                default:
+                    return(8);
+            }
+        };
+        setsingleLinePad('1vmin');
+        if((17<text)&&(text<40)){
+            return(4.6);
+        };
+        if((39<text)&&(text<41)){
+            return(4.2);
+        };
+        setsingleLinePad('0vmin');
+        if((40<text)&&(text<83)){
+            return(3.1);
+        };
+        if((text>82)&&(text<111)){
+           return(8/Math.sqrt(text/10) -0.05 )  
+        };
+        if((text>110)&&(text<185)){
+            return(2);
+        };
+        return(1.5);//the box has a 435 character limit 
+    }
+
+    
     function dfontSizing(event){
-        setTriageState(event.currentTarget.textContent);
+        setFontState(sizingFormula(event.currentTarget.textContent.length));
+        settextLengthState(event.currentTarget.textContent.length);
+    }
+    function downSizing(event){
         setFontState(fontState-0.1);
+        console.log(fontState.toPrecision(2)+'_'+textLengthState);
     }
     return (
         <div className='Triage_Bubble'>
-            <div className='wrapper-parent'>
-                <div class="Triage_Tag" contentEditable="true">{triageState}</div> 
-                <div class="Triage_Text" contentEditable="true" onKeyUp={dfontSizing} style={{fontSize:`${fontState}vmin`}}>{triageState}</div>
-            </div>
+                <div class="Triage_Tag" contentEditable="true" onClick={downSizing} style={{fontSize:'2.5vmin'}}>{textLengthState}_{fontState.toPrecision(2)}</div> 
+                <div id='message_container' class="Triage_Text" contentEditable="true" onKeyUp={dfontSizing} style={{'padding-top':`${singleLinePad}`, fontSize:`${fontState}vmin`}}>{triageTextState[triageState-1]}</div>
         </div>
         
     );
