358d3c5349de2795cc829ce1743b7d0df5b90ca3
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-04-27 13:11:20 +1000

    Triage Bubble v1

--- inept/src/components/triage_bbl.js
+++ inept/src/components/triage_bbl.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState, useRef } from 'react'
 import './triage_bbl.css';
-import { flushSync } from 'react-dom';
 
-var prevLines = [NaN,NaN];
-var prevDiffs = [NaN,NaN];
 var repeatOff = 0;
 var nL_adj = 0;
-var triageTextState= ['Priority High', 'Priority Mid', 'Priority eh'];
 
 function Triage_Bubble(props) {
-    const [triageState, setTriageState] = useState(1);
-    
+    const [triageState, setTriageState] = useState(0);
+    var triageTextState= [localStorage.getItem('Priority1'), localStorage.getItem('Priority2'),localStorage.getItem('Priority3')];
     const [paddState, setPaddState] = useState(10);
     const [fontState, setFontState] = useState(20);
 
-    
-    
-    React.useEffect(() => { autoResize();})//after each render
+    React.useEffect(() => { autoResize();
+    if(localStorage.getItem("Priority1") === null){ localStorage.setItem('Priority1','Highest Priority')}
+    if(localStorage.getItem("Priority2") === null){ localStorage.setItem('Priority2','Mid Priority')}
+    if(localStorage.getItem("Priority3") === null){ localStorage.setItem('Priority3','Eh Priority')}
+    })//after each render
 
     React.useEffect(() => {
         function handleResize() { autoResize();}
@@ -24,16 +22,15 @@ function Triage_Bubble(props) {
       })
 
     function textResponse(event){
+        JSON.parse(localStorage.setItem('Priority'+(triageState+1),document.getElementById('parent').childNodes[0].textContent))
         triageTextState[triageState] = document.getElementById('parent').childNodes[0].textContent;
-        console.log('saving');
+        console.log(triageTextState);
     }
     function changeTriage(event){
-        if(triageState==3){setTriageState(1);}else{setTriageState(triageState+1);}
+        if(triageState===2){setTriageState(0);}else{setTriageState(triageState+1);}
     }
 
     const autoSizerDebug = false;
-
-
     function autoResize() {
         var fontSZ = fontState;
         var ChildH = document.getElementById('parent').childNodes[0].offsetHeight;
@@ -43,18 +40,9 @@ function Triage_Bubble(props) {
         var delta = 0;
         if(autoSizerDebug) console.log('>' + fontState + ' _ '+ ParentH+' _ '  + ChildH + ' | ' + 0.5 * fontState + ' < ' + diff + ' < ' + 1.5 * fontSZ + ' | ' + paddState + '(' + (diff < 0.5 * fontSZ) + '||' + (diff > 1.5 * fontSZ)+')');
         if((diff < 0.5 * fontState)|| (diff > 1.5 * fontState)){
-            /* if(((prevDiffs[1]<0)&&(diff-0.5*fontState<0))&&((prevDiffs[1]<0)!=(prevDiffs[0]<0))){
-                nL_adj=1;
-                console.log('nLines Adjusted')
-            }else{
-                prevDiffs[1]=prevDiffs[0];
-                prevLines[1]=prevLines[0];
-                prevDiffs[0]=diff - 0.5 * fontState;
-                prevLines[0]=nLines;
-            } */
-            if(repeatOff>5){nL_adj+=1;console.log("{",nL_adj,"}")}
+            if(repeatOff>5){nL_adj+=1; if(autoSizerDebug)console.log("{",nL_adj,"}")}
             delta = (diff - fontState)/(nLines+nL_adj);//calculate the fontSize change
-            console.log(fontState,'+', delta)
+            if(autoSizerDebug) console.log(fontState,'+', delta)
             setFontState(fontState+delta);//apply
             fontSZ=fontState+delta;//what new fontsize would be (cannot rely on fontState cause it's async)
             setPaddState(Math.round(0.5*fontSZ)) //set ideal padding(will get fixed otherwise)
@@ -62,8 +50,6 @@ function Triage_Bubble(props) {
             repeatOff++;// the text has been adjusted, keep track of repeat offenders
         }else{
             repeatOff = 0;
-            prevLines = [NaN,NaN];
-            prevDiffs = [NaN,NaN];
             if((paddState<0.4*diff)||(paddState>0.6*diff)) setPaddState(0.5*diff)//set actual padding
         }
         if(autoSizerDebug) console.log('<' + fontSZ + ' _ '+ ParentH+' _ ' + ChildH + ' | ' + 0.5 * fontSZ + ' < ' + diff + ' < ' + 1.5 * fontSZ + ' | ' + paddState + '(' + (diff < 0.5 * fontSZ) + '||' + (diff > 1.5 * fontSZ) + ')');
@@ -72,10 +58,10 @@ function Triage_Bubble(props) {
     return (
         <div className='Triage_Bubble'>
                 <div class="Triage_Tag_Bubble" onClick={changeTriage} >
-                <div class="Triage_Tag_Text" >Triage #{triageState}</div>
+                <div class="Triage_Tag_Text" >Triage #{triageState+1}</div>
                 </div> 
             <div id='parent' class="Triage_Text" onKeyUpCapture={textResponse} style={{fontSize:fontState+'px', lineHeight:fontState*1+'px', paddingTop:paddState+'px'}} >
-                    <div class="Text_Holder" contentEditable="true">{triageTextState[triageState-1]}</div>
+                    <div class="Text_Holder" contentEditable="true">{triageTextState[triageState]}</div>
                 </div>
         </div>
         
