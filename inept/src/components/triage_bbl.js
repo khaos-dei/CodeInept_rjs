ffbdd57b820895b1d94a1a5818be24ce771dc6de
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-04-27 12:34:40 +1000

    Autosizing foundation

--- inept/src/components/triage_bbl.js
+++ inept/src/components/triage_bbl.js
@@ -4,26 +4,28 @@ import { flushSync } from 'react-dom';
 
 var prevLines = [NaN,NaN];
 var prevDiffs = [NaN,NaN];
+var repeatOff = 0;
+var nL_adj = 0;
+var triageTextState= ['Priority High', 'Priority Mid', 'Priority eh'];
 
 function Triage_Bubble(props) {
     const [triageState, setTriageState] = useState(1);
-    var triageTextState= ['Priority High', 'Priority Mid', 'Priority eh'];
     
     const [paddState, setPaddState] = useState(10);
     const [fontState, setFontState] = useState(20);
 
     
     
-    React.useEffect(() => { autoResize();})//for the first load
+    React.useEffect(() => { autoResize();})//after each render
+
     React.useEffect(() => {
         function handleResize() { autoResize();}
         window.addEventListener('resize', handleResize)//for the window being resized
       })
 
     function textResponse(event){
-        triageTextState[triageState] = event.currentTarget.textContent;
-        console.log('filler');
-        autoResize();
+        triageTextState[triageState] = document.getElementById('parent').childNodes[0].textContent;
+        console.log('saving');
     }
     function changeTriage(event){
         if(triageState==3){setTriageState(1);}else{setTriageState(triageState+1);}
@@ -34,33 +36,35 @@ function Triage_Bubble(props) {
 
     function autoResize() {
         var fontSZ = fontState;
-        var ChildH = document.getElementById('parent').childNodes[0].offsetHeight;;
+        var ChildH = document.getElementById('parent').childNodes[0].offsetHeight;
         var ParentH = document.getElementById('parent').clientHeight;
         var nLines = Math.round(ChildH / fontState);
         var diff = ParentH - ChildH;
         var delta = 0;
-
         if(autoSizerDebug) console.log('>' + fontState + ' _ '+ ParentH+' _ '  + ChildH + ' | ' + 0.5 * fontState + ' < ' + diff + ' < ' + 1.5 * fontSZ + ' | ' + paddState + '(' + (diff < 0.5 * fontSZ) + '||' + (diff > 1.5 * fontSZ)+')');
         if((diff < 0.5 * fontState)|| (diff > 1.5 * fontState)){
-            if(((prevDiffs[1]<0)&&(diff-0.5*fontState<0))&&((prevDiffs[1]<0)!=(prevDiffs[0]<0))){
-                nLines+=2;
+            /* if(((prevDiffs[1]<0)&&(diff-0.5*fontState<0))&&((prevDiffs[1]<0)!=(prevDiffs[0]<0))){
+                nL_adj=1;
                 console.log('nLines Adjusted')
             }else{
                 prevDiffs[1]=prevDiffs[0];
                 prevLines[1]=prevLines[0];
                 prevDiffs[0]=diff - 0.5 * fontState;
                 prevLines[0]=nLines;
-            }
-            delta = (diff - fontState)/nLines;//calculate the fontSize change
+            } */
+            if(repeatOff>5){nL_adj+=1;console.log("{",nL_adj,"}")}
+            delta = (diff - fontState)/(nLines+nL_adj);//calculate the fontSize change
             console.log(fontState,'+', delta)
             setFontState(fontState+delta);//apply
             fontSZ=fontState+delta;//what new fontsize would be (cannot rely on fontState cause it's async)
             setPaddState(Math.round(0.5*fontSZ)) //set ideal padding(will get fixed otherwise)
             if(autoSizerDebug) console.log("Padding: ",Math.round(0.5*fontSZ));
+            repeatOff++;// the text has been adjusted, keep track of repeat offenders
         }else{
+            repeatOff = 0;
             prevLines = [NaN,NaN];
             prevDiffs = [NaN,NaN];
-            setPaddState(0.5*diff)//set actual padding
+            if((paddState<0.4*diff)||(paddState>0.6*diff)) setPaddState(0.5*diff)//set actual padding
         }
         if(autoSizerDebug) console.log('<' + fontSZ + ' _ '+ ParentH+' _ ' + ChildH + ' | ' + 0.5 * fontSZ + ' < ' + diff + ' < ' + 1.5 * fontSZ + ' | ' + paddState + '(' + (diff < 0.5 * fontSZ) + '||' + (diff > 1.5 * fontSZ) + ')');
     }   
