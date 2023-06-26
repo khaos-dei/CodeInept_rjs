998f3d93c70e5568f48201acf16aa769be128ac0
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-06-26 12:42:30 +1000

    some icons added
    icons from https://www.flaticon.com/authors/basic-black/solid?author_id=159&type=standard

--- inept/src/components/notepad.js
+++ inept/src/components/notepad.js
@@ -1,6 +1,9 @@
 import './notepad.css';
 import React, { useRef, useState} from 'react';
 import Dialog from './dialogue'
+import FontSize_icn from '../assets/icn_FontSize.png'
+import AddNote_icn from '../assets/icn_AddNote.png'
+import NoteList_icn from '../assets/icn_NoteList.png'
 
 function Notepad(props) {
     var the_text = useRef('Saving notes is not yet implemented')
@@ -42,10 +45,10 @@ function Notepad(props) {
             <div className='Notebook_Edge' />
             <div contentEditable="true" suppressContentEditableWarning="true" className='Notebook_Text' style={{fontSize:fontSize+'px'}}>{the_text.current}</div>
             <div className='ButtonLine'>
-            <button className='SideButn' >+</button>
-            <button className='SideButn' onClick={manageDialog}>≣</button>
+            <button className='SideButn' ><img src={AddNote_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
+            <button className='SideButn' onClick={manageDialog}><img src={NoteList_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
             <button className='SideButn' onClick={SmallerFont}>Oo</button>
-            <button className='SideButn' onClick={BiggerFont}>oO</button>
+            <button className='SideButn' onClick={BiggerFont}><img src={FontSize_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
             </div>
              </div>
     );
