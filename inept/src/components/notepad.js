32ac8c905a31c2059c46b34e78c613acde6763f2
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-06-26 17:19:32 +1000

    button rearrange v3

--- inept/src/components/notepad.js
+++ inept/src/components/notepad.js
@@ -5,7 +5,11 @@ import React, { useRef, useState} from 'react';
 import FontSize_icn from '../assets/icn_FontSize.png'
 import AddNote_icn from '../assets/icn_AddNote.png'
 import NoteList_icn from '../assets/icn_NoteList.png'
-import FlippingArrows_icn from '../assets/icn_FlippingArrows.png'
+import FlippingArrows_icn from '../assets/icn_FlippingArrows2.png'
+import OnesArrow_icn from '../assets/icn_OnesArrow.png'
+import Delete_icn from '../assets/icn_Delete.png'
+import Colors_icn from '../assets/icn_Colors.png'
+import AlgLeft_icn from '../assets/icn_AlgLeft.png'
 
 
 import Dialog from './dialogue'
@@ -48,25 +52,29 @@ function Notepad(props) {
 
 
             <div className='Notebook_TopLine' />
+            <div className='TabArr'> 
+                <button className='ArrButn'> <img src={FlippingArrows_icn} alt="<<" style={{width:"4vmin", filter:'invert(1)', transform:'rotate(180deg)'}}/> </button>
+                <button className='ArrButn'> <img src={OnesArrow_icn} alt="<" style={{width:"2vmin", filter:'invert(1)', transform:'rotate(180deg)'}}/> </button>
+                <button className='ArrButn' ><img src={AddNote_icn} alt="+" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
+                <button className='ArrButn' onClick={manageDialog}><img src={NoteList_icn} alt="=" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
+                <button className='ArrButn' ><img src={Delete_icn} alt="del" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
+                <button className='ArrButn' ><img src={Colors_icn} alt="col" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
+                <button className='ArrButn'> <img src={OnesArrow_icn} alt=">" style={{width:"2vmin", filter:'invert(1)'}}/> </button>
+                <button className='ArrButn'> <img src={FlippingArrows_icn} alt=">>" style={{width:"4vmin", filter:'invert(1)'}}/> </button>
+            </div>
             <div className='TabLine'>
                 <button className='Tab'>Day</button>
                 <button className='Tab'>Week</button>
                 <button className='Tab'>Month</button>
             </div>
-
-            <div className='Notebook_Edge' />
             <div contentEditable="true" suppressContentEditableWarning="true" className='Notebook_Text' style={{fontSize:fontSize+'px'}}>{the_text.current}</div>
+            
             <div className='SideButtonLine'>
-            <div className='TabArr'> 
-            <button className='ArrButn'> <img src={FlippingArrows_icn} alt="Oo" style={{width:"1.5vmin", filter:'invert(1)', transform:'rotate(180deg)'}}/> </button>
-            <button className='ArrButn'> <img src={FlippingArrows_icn} alt="Oo" style={{width:"1.5vmin", filter:'invert(1)'}}/> </button>
-            </div>
-            <button className='SideButn' ><img src={AddNote_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
-            <button className='SideButn' onClick={manageDialog}><img src={NoteList_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
-            <button className='SideButn' onClick={SmallerFont}>Oo</button>
+            <button className='SideButn' onClick={BiggerFont}><img src={AlgLeft_icn} alt=">>" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
             <button className='SideButn' onClick={manageMiniDialog}><img src={FontSize_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
             </div>
-             </div>
+            
+            </div>
     );
   }
   
