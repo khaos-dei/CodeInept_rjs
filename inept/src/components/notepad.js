444ea1c5113b5186737293f85653f300beb65fd3
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-06-28 18:00:48 +1000

    headings menu

--- inept/src/components/notepad.js
+++ inept/src/components/notepad.js
@@ -6,17 +6,7 @@ import TextStyle from '@tiptap/extension-text-style'
 import { BubbleMenu, EditorContent, FloatingMenu, fromString, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
-import FontSize_icn from '../assets/icn_FontSize.png'
-import AddNote_icn from '../assets/icn_AddNote.png'
-import NoteList_icn from '../assets/icn_NoteList.png'
-import FlippingArrows_icn from '../assets/icn_FlippingArrows2.png'
-import OnesArrow_icn from '../assets/icn_OnesArrow.png'
-import Delete_icn from '../assets/icn_Delete.png'
-import Colors_icn from '../assets/icn_Colors.png'
-import AlgLeft_icn from '../assets/icn_AlgLeft.png'
-import BiggerFont_icn from '../assets/icn_BiggerFont.png'
-import SmallerFont_icn from '../assets/icn_SmallerFont.png'
-
+import {Icons,Alts} from '../constants/Icons'
 
 import Dialog from './dialogue'
 import MiniDialog from './mini_dialogue'
@@ -25,7 +15,8 @@ function Notepad(props) {
     var the_text = useRef('Saving notes is not yet implemented')
     const [fontSize, setfontSize] = useState(12);
     const [showDialog, setShowDialog] = useState(false);
-    const [showMiniDialog, setShowMiniDialog] = useState(false);
+    const [showMiniDialogFnt, setShowMiniDialogFnt] = useState(false);
+    const [showMiniDialogH0, setShowMiniDialogH0] = useState(false);
 
     
     const editor = useEditor({
@@ -61,23 +52,26 @@ function Notepad(props) {
         `,
       })
     
-    const manageMiniDialog = () => {
-        setShowMiniDialog(!showMiniDialog);
+    const manageMiniDialogFnt = () => {
+      setShowMiniDialogFnt(!showMiniDialogFnt);
     }
+    const manageMiniDialogH0 = () => {
+      setShowMiniDialogH0(!showMiniDialogH0);
+  }
     const manageDialog = () => {
         setShowDialog(!showDialog);
     }
 
     function BiggerFont() {
         setfontSize(fromString(fontSize)+2);
-    }
+      }
     function SmallerFont() {
         setfontSize(fromString(fontSize)-2);
-    }
+      }
 
 
-    function Icon(img, alt, size, deg=0){
-       return(<img src={img} alt={alt} style={{width:size, filter:'invert(1)', transform:'rotate('+deg+'deg)'}}/>)
+    function Icon( key, size, deg=0){
+       return(<img src={Icons[key]} alt={Alts[key]} style={{width:size, filter:'invert(1)', transform:'rotate('+deg+'deg)'}}/>)
     }
     return (
         <div className='Notepad'>
@@ -107,35 +101,52 @@ function Notepad(props) {
                 open={showDialog}
                 callback={manageDialog} />
             <MiniDialog
-                header="Choose a font"
                 body={
                   <div className='FontButtonLine'>
-                  <input name="FontSizeInpt" style={{height:"3vmin", width:"100%"}}  value={fontSize} type="number" onInput={e => setfontSize(e.target.value)}></input>
-                  <p>px</p>
-                  <button className='h1Butn' onClick={BiggerFont}> {Icon(BiggerFont_icn,"Up","3vmin",0)}</button>
-                  <button className='h1Butn' onClick={SmallerFont}> {Icon(SmallerFont_icn,"Down","3vmin",0)}</button>
+                  <input name="FontSizeInpt" style={{height:"3vmin", width:"100%", margin:"0%"}}  value={fontSize} type="number" onInput={e => setfontSize(e.target.value)}></input>
+                  <div style={{padding:"0%"}}>px</div>
+                  <button className='h1Butn' onClick={BiggerFont}> {Icon("FontL","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={SmallerFont}> {Icon("FontS","3vmin",0)}</button>
                   </div>
                 }
-                open={showMiniDialog}
+                open={showMiniDialogFnt}
                 top={580}
                 left={800}
                 height={"5vmin"}
                 width={"18vmin"}
                 background_color="gray"
-                callback={manageMiniDialog} />
-
+                callback={manageMiniDialogFnt} />
+            <MiniDialog
+                body={
+                  <div className='HeadingButtonLine'>
+                  <button className='h1Butn'> {Icon("P","3vmin",0)}</button>
+                  <button className='h1Butn'> {Icon("H1","3vmin",0)}</button>
+                  <button className='h1Butn'> {Icon("H2","3vmin",0)}</button>
+                  <button className='h1Butn'> {Icon("H3","3vmin",0)}</button>
+                  <button className='h1Butn'> {Icon("H4","3vmin",0)}</button>
+                  <button className='h1Butn'> {Icon("H5","3vmin",0)}</button>
+                  <button className='h1Butn'> {Icon("H6","3vmin",0)}</button>
+                  </div>
+                }
+                open={showMiniDialogH0}
+                top={580}
+                left={800}
+                height={"9vmin"}
+                width={"14vmin"}
+                background_color="gray"
+                callback={manageMiniDialogH0} />
 
             <div className='Notebook_TopLine' />
             <div className='TabArr'> 
-                <button className='h1Butn'> {Icon(FlippingArrows_icn,"<<","4vmin",180)} </button>
-                <button className='h1Butn'> {Icon(OnesArrow_icn,"<","2vmin",180)} </button>
-                <button className='h1Butn' >{Icon(AddNote_icn,"+","2.5vmin")}</button>
+                <button className='h1Butn'> {Icon("Arrows","4vmin",180)} </button>
+                <button className='h1Butn'> {Icon("Arrow","2vmin",180)} </button>
+                <button className='h1Butn' >{Icon("Add","2.5vmin")}</button>
                 <button className='h1Butn' onClick={manageDialog}>
-                                            {Icon(NoteList_icn,"=","2.5vmin")}</button>
-                <button className='h1Butn' >{Icon(Delete_icn,"del","2.5vmin")}</button>
-                <button className='h1Butn' >{Icon(Colors_icn,"col", "2.5vmin")}</button>
-                <button className='h1Butn'> {Icon(OnesArrow_icn,">","2vmin")} </button>
-                <button className='h1Butn'> {Icon(FlippingArrows_icn,">>","4vmin")} </button>
+                                            {Icon("List","2.5vmin")}</button>
+                <button className='h1Butn' >{Icon("Delete","2.5vmin")}</button>
+                <button className='h1Butn' >{Icon("Color", "2.5vmin")}</button>
+                <button className='h1Butn'> {Icon("Arrow","2vmin")} </button>
+                <button className='h1Butn'> {Icon("Arrows","4vmin")} </button>
             </div>
             <div className='TabLine'>
                 <button className='Tab'>Day</button>
@@ -147,8 +158,10 @@ function Notepad(props) {
             </div>
             
             <div className='SideButtonLine'>
-            <button className='SideButn' onClick={BiggerFont}><img src={AlgLeft_icn} alt=">>" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
-            <button className='SideButn' onMouseEnter={manageMiniDialog}><img src={FontSize_icn} alt="Oo" style={{width:"2.5vmin", filter:'invert(1)'}}/></button>
+            <button className='SideButn' onClick={BiggerFont}>{Icon("AlignL","2.5vmin")}</button>
+            <button className='SideButn' onMouseDown={manageMiniDialogFnt}>{Icon("Font","2.5vmin")}</button>
+            <button className='SideButn'>{Icon("AddList","2.5vmin")}</button>
+            <button className='SideButn' onMouseDown={manageMiniDialogH0}>{Icon("H0","2.5vmin")}</button>
             </div>
             
             </div>
---
+++ inept/src/constants/Icons.js
@@ -0,0 +1,85 @@
+import FontSize_icn from '../assets/icn_FontSize.png'
+import AddNote_icn from '../assets/icn_AddNote.png'
+import NoteList_icn from '../assets/icn_NoteList.png'
+import FlippingArrows_icn from '../assets/icn_FlippingArrows2.png'
+import OnesArrow_icn from '../assets/icn_OnesArrow.png'
+import Delete_icn from '../assets/icn_Delete.png'
+import Colors_icn from '../assets/icn_Colors.png'
+import AlgLeft_icn from '../assets/icn_AlgLeft.png'
+import BiggerFont_icn from '../assets/icn_BiggerFont.png'
+import SmallerFont_icn from '../assets/icn_SmallerFont.png'
+import AddList_icn from '../assets/icn_AddList.png'
+import Heading_icn from '../assets/icn_Heading.png'
+import Paragraph_icn from '../assets/icn_Paragraph.png'
+import Heading1_icn from '../assets/icn_Heading1.png'
+import Heading2_icn from '../assets/icn_Heading2.png'
+import Heading3_icn from '../assets/icn_Heading3.png'
+import Heading4_icn from '../assets/icn_Heading4.png'
+import Heading5_icn from '../assets/icn_Heading5.png'
+import Heading6_icn from '../assets/icn_Heading6.png'
+import ListBullet_icn from '../assets/icn_ListBullet.png'
+import ListCheck_icn from '../assets/icn_ListCheck.png'
+import ListNum_icn from '../assets/icn_ListNum.png'
+
+
+
+const Icons  = {
+    "Font":FontSize_icn,
+    "Add": AddNote_icn,
+    "List":NoteList_icn,
+
+    "Arrows":FlippingArrows_icn,
+    "Arrow": OnesArrow_icn,
+    "Delete":Delete_icn,
+    "Color":Colors_icn,
+    "AlignL": AlgLeft_icn,
+    "FontL":BiggerFont_icn,
+    "FontS":SmallerFont_icn,
+
+    "H0":Heading_icn,
+    "P":Paragraph_icn,
+    "H1":Heading1_icn,
+    "H2":Heading2_icn,
+    "H3":Heading3_icn,
+    "H4":Heading4_icn,
+    "H5":Heading5_icn,
+    "H6":Heading6_icn,
+
+    
+    "AddList":AddList_icn,
+    "Bullet":ListBullet_icn,
+    "Enum":ListNum_icn,
+    "Todo":ListCheck_icn,
+  };
+
+const Alts  = {
+    "Font":"Tt",
+    "Add": "+",
+    "List":"=",
+
+    "Arrows":">>",
+    "Arrow": ">",
+    "Delete":"del",
+    "Color":"col",
+    "AlignL": "left",
+    "FontL":"AA",
+    "FontS":"aa",
+    "AddList":"L+",
+
+    "H0":"H0",
+    "P":"P",
+    "H1":"h1",
+    "H2":"h2",
+    "H3":"h3",
+    "H4":"h4",
+    "H5":"h5",
+    "H6":"h6",
+    
+    "Bullet":"*-",
+    "Enum":"1-",
+    "Todo":"[]-",
+  };
+
+
+
+export {Icons,Alts}
