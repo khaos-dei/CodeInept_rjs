4cfb1f820d013f26e78c46e6ba3d86eb32a6731e
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-06-29 10:00:07 +1000

    heading buttons functioning

--- inept/src/components/notepad.js
+++ inept/src/components/notepad.js
@@ -154,13 +154,13 @@ function Notepad(props) {
             <MiniDialog /* Heading types Buttons */
                 body={
                   <div className='HeadingButtonLine'>
-                  <button className='h1Butn'> {Icon("P","3vmin",0)}</button>
-                  <button className='h1Butn'> {Icon("H1","3vmin",0)}</button>
-                  <button className='h1Butn'> {Icon("H2","3vmin",0)}</button>
-                  <button className='h1Butn'> {Icon("H3","3vmin",0)}</button>
-                  <button className='h1Butn'> {Icon("H4","3vmin",0)}</button>
-                  <button className='h1Butn'> {Icon("H5","3vmin",0)}</button>
-                  <button className='h1Butn'> {Icon("H6","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().setParagraph().run()}> {Icon("P","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}> {Icon("H1","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}> {Icon("H2","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}> {Icon("H3","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}> {Icon("H4","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}> {Icon("H5","3vmin",0)}</button>
+                  <button className='h1Butn' onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}> {Icon("H6","3vmin",0)}</button>
                   </div>
                 }
                 open={showMiniDialogH0} callback={manageMiniDialogH0} 
