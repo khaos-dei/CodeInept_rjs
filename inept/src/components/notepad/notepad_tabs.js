7314bb718749ec79e52bed824528e782adceb445
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-06-30 14:29:55 +1000

    Adding a New Note 1

--- inept/src/components/notepad/notepad_tabs.js
+++ inept/src/components/notepad/notepad_tabs.js
@@ -1,38 +1,60 @@
 import './notepad_tabs.css';
 import React, { useState} from 'react';
-import Dialog from '../dialogue'
 import {Icon} from '../../constants/Icons'
 import {setToLS, getFromLS} from '../localstorage_component';
-
+import MiniDialog from '../mini_dialogue'
+import Dialog from '../dialogue'
 import NotepadTabMenu from './notepad_tab_menu'
 
 
 function NotepadTabs(props) {
     const [showDialog, setShowDialog] = useState(false);
+    const [showAddDialog, setShowAddDialog] = useState(false);
+    
     const [activeTab, setactiveTab]= useState(getFromLS("active-Tab"));
     const [firstTab, setfirstTab]= useState(getFromLS("first-Tab"));
+
     const [noteList, setnoteList]= useState(getFromLS("note-List"));
     
+    const [newName, setnewName] = useState('New Note');
 
+
+    function SaveNewNote(){
+      setToLS("note-List",[...noteList, newName]);
+      setnoteList(noteList => [...noteList, newName]);
+      /* setnoteList(noteList.push(newName));  */
+      setShowAddDialog(!showAddDialog)
+  }
     const manageDialog = () => {
         setShowDialog(!showDialog);
-        console.log(firstTab+1);
     }
-
     return (
       <>
         <Dialog 
             header="Notes List" 
             body={<NotepadTabMenu Tabs={noteList}/>} 
             width={50}
-            open={showDialog} callback={manageDialog}/>
+            open={showDialog} callback={manageDialog}
+            newName={[newName, setnewName]}
+            />
+        <MiniDialog /* Font Size Buttons Working! */
+          body={
+            <div className='FontButtonLine'>
+            <input name="NewNameInpt" style={{height:"3vmin", width:"100%", margin:"0%"}} value={newName} onInput={e => setnewName(e.target.value)}></input>
+            <button className='IcnBtn' onClick={SaveNewNote}> Save</button>
+            <button className='IcnBtn' onClick={() =>setShowAddDialog(!showAddDialog)}> Cancel</button>
+            </div>
+          }
+          open={showAddDialog}
+          position={["3%","70%","5vmin","28vmin"]}
+          background_color="gray"/>
 
         <div className='Notebook_TopLine' />
           
         <div className='TabArr'> 
             <button className='IcnBtn' onClick={()=>setfirstTab(Number(firstTab)-3)}> {Icon("Arrows","4vmin",180)} </button>
             <button className='IcnBtn' onClick={()=>setfirstTab(Number(firstTab)-1)}> {Icon("Arrow","2vmin",180)} </button>
-            <button className='IcnBtn' >{Icon("Add","2.5vmin")}</button>
+            <button className='IcnBtn' onClick={() =>setShowAddDialog(!showAddDialog)}>{Icon("Add","2.5vmin")}</button>
             <button className='IcnBtn' onClick={manageDialog}> {Icon("List","2.5vmin")}</button>
             <button className='IcnBtn' >{Icon("Delete","2.5vmin")}</button>
             <button className='IcnBtn' onClick={()=>setfirstTab(Number(activeTab)-1)} >{Icon("Seek", "2.5vmin")}</button>
