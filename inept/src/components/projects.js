f78d40687ee9128fafd2fd0bbb8470891ee5de57
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-07-06 11:20:12 +1000

    progress bars basics

--- inept/src/components/projects.js
+++ inept/src/components/projects.js
@@ -4,7 +4,12 @@ function Projects(props) {
 
     return(
         <div className='Projects'>
-            <p>here go the projects</p>
+            <div className='Projectsa'>
+                <div className='ProgressBar'><div className='ProgressColor' style={{ width:"50%"}}/></div>
+                <div className='ProgressBar'><div className='ProgressColor' style={{ width:"25%"}}/></div>
+                <div className='ProgressBar'><div className='ProgressColor' style={{ width:"80%"}}/></div>
+                <div className='ProgressBar'><div className='ProgressColor'/></div>
+            </div>
         </div>
     );
   }
