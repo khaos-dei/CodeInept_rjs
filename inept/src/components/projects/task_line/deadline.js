e76f130f10e1b842ec0838da53ba054fe8a050d5
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-07-31 15:17:47 +1000

    deadlines can be cancelled

--- inept/src/components/projects/task_line/deadline.js
+++ inept/src/components/projects/task_line/deadline.js
@@ -1,4 +1,5 @@
 
+import "../tasks_menu.css"
 import React, { useState, forwardRef} from 'react';
 import IconButton from 'utils/IconButton';
 import {IconNoButton} from 'utils/IconButton';
@@ -13,15 +14,16 @@ function DeadLine({ ddl, ddl_feedback}) {
     const [pick, showPick] = useState(false);
     
     function status_icon(){
-        if(ddl < long_ago)
-            return(<IconNoButton is="WarnBad" size='2.5vmin' color='ad0000' style={{marginRight: "0.5vmin"}}/>)
+    if(ddl < long_ago)
+            return(<IconNoButton is="WarnBad" size='2.5vmin' color='ad0000' style={{marginRight: "0.5vmin", marginTop:"0.5vmin"}}/>)
         if(ddl < now)
-            return(<IconNoButton is="Warn" size='2.5vmin' color='ed6802' style={{marginRight: "0.5vmin"}}/>)
-        return(<IconNoButton is="Time" size='2.5vmin' color='black' style={{marginRight: "0.5vmin"}}/> )
+            return(<IconNoButton is="Warn" size='2.5vmin' color='ed6802' style={{marginRight: "0.5vmin", marginTop:"0.5vmin"}}/>)
+        
+        return(<IconNoButton is="Time" size='2.5vmin' color='black' style={{marginRight: "0.5vmin", marginTop:"0.5vmin"}}/> )
     }
     const ExistingInput = forwardRef(({ onClick }, ref) => (
         <button className="DeadlineDate" onClick={onClick} ref={ref}>
-            {status_icon()}&nbsp;{write_date(ddl)}&nbsp;
+            {status_icon()}&nbsp;{write_date(ddl)}
         </button>
       ));
 
@@ -32,11 +34,12 @@ function DeadLine({ ddl, ddl_feedback}) {
         ));
 
     return (
-            <div className='TaskDDL'>
-                
-                {pick &&<IconButton is="No" size='2.5vmin' color='blue' onClick={ddl_feedback(false)}   style={{marginRight: "0.5vmin"}}/>}
-                <DatePicker showPopperArrow={false} selected={ddl} onChange={(date) => {ddl=date; ddl_feedback(date)}} customInput={ddl?<ExistingInput />:<NewInput />} onCalendarOpen={() => {showPick(true)}} onCalendarClose={() => showPick(false)}/>
-                
+            <div className='TaskDDL' onMouseOverCapture={()=>{showPick(true)}} onMouseLeave={()=>{showPick(false)}}>
+                <div className="DatepickerDiv">
+                <DatePicker showPopperArrow={false} selected={ddl} onChange={(date) => {ddl=date; ddl_feedback(date)}} customInput={ddl?<ExistingInput />:<NewInput />} onCalendarOpen={() => {showPick(!pick)}} onCalendarClose={() => showPick(!pick)} />
+                {ddl && pick && <IconButton is="No" size='1.6vmin' color='blue' onClick={()=>{ddl_feedback(false)}}   style={{position: "relative", top:"-30%"}}/>} 
+                </div>
+                  
             </div>
             );
     }
