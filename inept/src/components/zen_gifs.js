fd23b897ba8266fc2b559a486f8617488e24de57
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-08-09 17:04:07 +1000

    Gif choosing menu foundation

--- inept/src/components/zen_gifs.js
+++ inept/src/components/zen_gifs.js
@@ -1,15 +1,26 @@
 import './zen_gifs.css';
 import gif1 from '../assets/424-breathing.gif'
 import gif2 from '../assets/Zen_Garden.gif'
+import Dialog from 'utils/dialogue';
+import ZenMenu from './zengif_menu';
+import React, { useState, useEffect} from 'react';
 
 function ZenGifs(props) {
+    const [showDialogue, setDialogue] = useState(false);
 
     return(
-        <img src={gif1} alt="It should be a gif here..." className='SquareGif'/>
-        /* <div className='ZenGifs'> 
-            
-            <img src={gif2} alt="It should be a gif here..." style={{width:"50%"}}/>
-         </div> */
+        <>
+        <Dialog /* Delete Project? */
+            header={"Choose a GIF"}
+            body={<ZenMenu/>}
+            open={showDialogue}
+            callback={setDialogue}
+            width={90}
+            background_color="gray"
+            />
+
+        <img onClick={()=>{setDialogue(!showDialogue)}} src={gif1} alt="It should be a gif here..." className='SquareGif'/>
+        </>
     );
   }
   
---
+++ inept/src/components/zengif_menu.js
@@ -0,0 +1,49 @@
+import './zen_gifs.css';
+import React, {useState} from 'react';
+import IconButton from 'utils/IconButton';
+import gif1 from '../assets/424-breathing.gif'
+import gif2 from '../assets/Zen_Garden.gif'
+
+function ZenMenu({id, feedback}) {
+
+    return(
+        <>
+        <div className='Gif_Grid'>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+            <img src={gif1} alt="It should be a gif here..." className='Gif_In_Grid'/>
+        </div>
+        </>
+    );
+  }
+  
+  export default ZenMenu;
