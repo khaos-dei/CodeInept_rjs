ee15ce1909a3a26e69bfcc2f8b3d52b8e4c70106
Deidara Khariton <67588449+khaos-dei@users.noreply.github.com>
2023-08-11 16:09:24 +1000

    Adding youtube
    cause plans have changed

--- inept/src/components/zen_gifs.js
+++ inept/src/components/zen_gifs.js
@@ -3,16 +3,26 @@ import {gifs} from 'constants/gifs'
 import Dialog from 'utils/dialogue';
 import ZenMenu from './zengif_menu';
 import React, { useState, useEffect} from 'react';
+import YouTube from 'react-youtube';
+import IconButton from 'utils/IconButton';
 
 function ZenGifs(props) {
     const [showDialogue, setDialogue] = useState(false);
     const [activeGif, setActiveGif] = useState(1);
+    const opts = {
+        height: '180vmin',
+        width: '304vmin',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 0,
+        },
+      };
 
     return(
         <>
         <Dialog /* Delete Project? */
             header={"Choose a GIF"}
-            body={<ZenMenu id={activeGif} feedback={setActiveGif} gifs={gifs}/>}
+            body={<ZenMenu id={activeGif} feedback={setActiveGif} gifs={gifs} closeback={setDialogue}/>}
             open={showDialogue}
             callback={setDialogue}
             width={90}
@@ -20,6 +30,10 @@ function ZenGifs(props) {
             />
 
         <img onClick={()=>{setDialogue(!showDialogue)}} src={gifs[activeGif]} alt="It should be a gif here..." className='SquareGif'/>
+        <div className='yt_container'>
+        <YouTube videoId="2g811Eo7K8U" opts={opts} />
+        </div>
+        <IconButton is="Settings" size='3vmin' visible={true} style={{position:"absolute", left:"94%", bottom:"82%"}}/>
         </>
     );
   }
--- inept/src/components/zengif_menu.js
+++ inept/src/components/zengif_menu.js
@@ -2,14 +2,14 @@ import './zen_gifs.css';
 import React, {useState} from 'react';
 import IconButton from 'utils/IconButton';
 
-function ZenMenu({id, feedback, gifs}) {
+function ZenMenu({id, feedback, gifs, closeback}) {
     
 
     function gif_options(active){
         console.log(active);
         let content=[];
         for(let i=0; i<gifs.length;i++){
-            content.push(<img src={gifs[i]} onClick={()=>{feedback(i)}} alt="It should be a gif here..." className='Gif_In_Grid' style={{backgroundColor:(i==active?'lightblue':'white')}} />)
+            content.push(<img src={gifs[i]} onClick={()=>{feedback(i); closeback(false);}} alt="It should be a gif here..." className='Gif_In_Grid' style={{backgroundColor:(i==active?'lightblue':'white')}} />)
         }
         return content;
     } 
